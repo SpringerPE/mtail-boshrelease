6969646857c2455ea25f3f7c4548e9cbd82ea8b2
Jose Riguera <jriguera@gmail.com>
2022-06-16 13:53:11 +0200

    rename git lfs blobstore

--- config/bucket.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Create the bucket for blobs and setup public read access
-BUCKET=$(cat final.yml | awk '/bucket_name:/{ print $2 }')
-
-s3cmd mb "s3://${BUCKET}"
-sleep 5
-s3cmd setacl "s3://${BUCKET}" --acl-public --recursive
